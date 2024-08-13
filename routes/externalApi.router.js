9a8c9078dc1efda775181d21e444e1c1fa0ecb3c
Memoryman2022 <rlacannon@gmail.com>
2024-08-13 23:47:49 +0200

    TEST

--- routes/externalApi.router.js
+++ routes/externalApi.router.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const router = express.Router(); // Corrected the syntax
+const router = express.Router();
 const axios = require("axios");
 
 // Replace with your actual API key
-const EXTERNAL_API_KEY = process.env.EXTERNAL_API_KEY; // Make sure to store this securely
+const EXTERNAL_API_KEY = process.env.EXTERNAL_API_KEY; // Store this securely
 
-// External API base URL
-const API_URL = "https://api.football-data.org/v4";
-
-// Define the route that the frontend will call to get standings
+// Define the route that the frontend will call
 router.get("/competitions/2021/standings", async (req, res) => {
   try {
-    const response = await axios.get(`${API_URL}/competitions/2021/standings`, {
-      headers: {
-        "X-Auth-Token": EXTERNAL_API_KEY,
-      },
-    });
+    const response = await axios.get(
+      "https://api.football-data.org/v4/competitions/2021/standings",
+      {
+        headers: {
+          "X-Auth-Token": EXTERNAL_API_KEY,
+        },
+      }
+    );
     res.json(response.data); // Send data to frontend
   } catch (error) {
     console.error("Error fetching data from external API:", error);
@@ -23,14 +23,17 @@ router.get("/competitions/2021/standings", async (req, res) => {
   }
 });
 
-// Define the route that the frontend will call to get match days for the calendar
+// Match days for calendar
 router.get("/competitions/2021/matches", async (req, res) => {
   try {
-    const response = await axios.get(`${API_URL}/competitions/2021/matches`, {
-      headers: {
-        "X-Auth-Token": EXTERNAL_API_KEY,
-      },
-    });
+    const response = await axios.get(
+      "https://api.football-data.org/v4/competitions/2021/matches",
+      {
+        headers: {
+          "X-Auth-Token": EXTERNAL_API_KEY,
+        },
+      }
+    );
     res.json(response.data); // Send data to frontend
   } catch (error) {
     console.error("Error fetching fixtures from external API:", error);
