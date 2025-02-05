4400a92a26bd53c0f197cd7e4d854cff65fcf2cb
Memoryman2022 <rlacannon@gmail.com>
2025-02-05 14:23:09 +0100

    return to old api url

--- utils/UpdateResults.js
+++ utils/UpdateResults.js
@@ -4,8 +4,7 @@ const axios = require("axios");
 const triggerUpdateResults = async () => {
   try {
     const response = await axios.get(
-      // "https://eplbackend.adaptable.app/api/updateResults",
-      "https://eplbackend.vercel.app/api/updateResults"
+      "https://eplbackend.adaptable.app/api/updateResults"
     );
     console.log("Update Results Response:", response.data);
   } catch (error) {
